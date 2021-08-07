a8d4c33cf743d6e1bfb6885894daa91fd33c9a70
Alireza-eb <88576230+Alireza-eb@users.noreply.github.com>
2021-08-07 16:19:33 +0430

    Update database.go

--- database.go
+++ database.go
@@ -1,48 +1,24 @@
 package main
 
 import (
-  "gorm.io/gorm"
-  "gorm.io/driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
-type Product struct {
-  gorm.Model
-  Code  string
-  Price uint
-}
-
 func main() {
-  db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-  if err != nil {
-    panic("failed to connect database")
-  }
+	db, err := gorm.Open("postgres", "user=gorm password=gorm dbname=gorm sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-  db.AutoMigrate(&Product{})
+	dbase := db.DB()
+	defer dbase.Close()
 
-  db.Create(&Product{Code: "D42", Price: 100})
+	err = dbase.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
 
-  
-  var product Product
-  db.First(&product, 1) 
-  db.First(&product, "code = ?", "D42") 
-
-  
-  db.Model(&product).Update("Price", 200)
-  
-  db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
-  db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-
-  db.Delete(&product, 1)
-  type User struct {
-	ID           1
-	Name         shop_db
-	Email        A@gmail.com
-	Age          19
-	Birthday     *time.Time
-	MemberNumber sql.NullString
-	ActivatedAt  sql.NullTime
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-  }
+	println("Connection to database established")
 }
